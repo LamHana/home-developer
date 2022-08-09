c8739a7f379fabefa39be6ecc1eace8d430bdb6a
Nam <duonghoangnam503@gmail.com>
2022-08-09 15:37:48 +0700

    add js for header

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,5 +11,17 @@ function scrollNav() {
         document.getElementById("header__nav-login-btn").style.padding = "6px 2px 6px 2.5px";
     }
 }
-
 window.addEventListener('scroll', scrollNav);
+
+var showNav = document.querySelector('.header__btn-menu');
+var ul = document.querySelector('.header__nav-container');
+showNav.addEventListener("click", function () {
+    ul.classList.toggle('display-flex');
+});
+
+var listLi = document.querySelectorAll('.list-item');
+listLi.forEach((item) => {
+    item.onclick = function () {
+        ul.classList.remove('display-flex');
+    };
+});
